7595c640d080c2f6ec9dd39fd2fe19d7c8642c82
Tocka Ayman <tocka.ayman@outlook.com>
2021-11-16 14:33:33 +0200

    Convert JokeService to interface and implementation closes #2

--- src/main/java/com/example/jokes/service/JokeService.java
+++ src/main/java/com/example/jokes/service/JokeService.java
@@ -1,14 +1,6 @@
 package com.example.jokes.service;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.stereotype.Service;
+public interface JokeService {
 
-@Service
-public class JokeService {
-
-    private final ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
-
-    public String getJoke() {
-        return chuckNorrisQuotes.getRandomQuote();
-    }
+    String getJoke();
 }
---
+++ src/main/java/com/example/jokes/service/JokeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.jokes.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
